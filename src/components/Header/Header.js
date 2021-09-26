502ebc06138081925ac913f5ddceff3e05c481f9
rakibshakib <rakibshakib74@gmail.com>
2021-09-26 14:07:54 +0600

    update bg color in cart and header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <h2 className='brand-name'>Investment Collection</h2>
             <p className="name-details">Investment Collection for Annual Cultural Program - 2021</p>
             <h3 className='total-budget'>Total Budget: $<span className='budget'>100,000</span></h3>
